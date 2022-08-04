d6f3ac1bf1b4443333818b5e344be1d0f8152054
myprojects <987384390@qq.com>
2022-08-04 11:11:02 +0800

    1.2.220804

--- a.py
+++ a.py
@@ -1,34 +1,37 @@
+from os import getcwd
 import PySimpleGUI as sg
 import random
-import pyperclip
 import datetime
 from Steper_class import Steper
 """
-    TODO 加载时选中最大值文本
+    DONE 加载时选中最大值文本
     TODO 把文件导入导出功能做出来
     TODO 优化界面
     TODO 代码优化
+    TODO 复制按钮删掉，文本居中
+    TODO 缩小放大时选中最大值文本
 """
 
 sg.theme('Topanga')   # Add a touch of color
 # All the stuff inside your window.
 Asteper = Steper()
-
-randomText = sg.Text('0')
+randomText = sg.Text('0', size=20, justification='center')
 timeText = sg.Text('2022年5月27日 000000')
 stepText = sg.Text('1')
 treedata = sg.TreeData()
-pathText = sg.Text(Asteper.getCurrentPath())
-minInputText = sg.InputText("1",size=10)
-maxInputText = sg.InputText("1",size=10, focus=True)
-layout = [[minInputText, sg.Button('生成',size=5, k="generate", bind_return_key=True), sg.Button('置步', k="restep"), maxInputText],
-          [randomText, sg.Button('复制', k="copy")],
-          [pathText],
-          [sg.Button('__dict__', k="print_dict")],
+mapText = sg.Text(Asteper.getCurrentPath())
+minInputText = sg.InputText("1", size=10)
+maxInputText = sg.InputText("1", size=10, focus=True)
+layout = [[minInputText, sg.Button('生成', size=5, k="generate", bind_return_key=True), sg.Button('置步', k="restep"), maxInputText],
+          [randomText],
+          [mapText],
+          [sg.Button('__dict__', k="print_dict"), sg.FileBrowse('导入', file_types=(("JSON file", "*.json"), ('All file', '*.*')), initial_folder=getcwd(), k="import"),
+           sg.FileSaveAs('导出', file_types=(("JSON file", "*.json"), ('All file', '*.*')), initial_folder=getcwd(), k="export", target="port_path")],
           [timeText]]
 
 # Create the Window
-window = sg.Window('Window Title', layout)
+window = sg.Window('Window Title', layout, finalize=True)
+maxInputText.Widget.select_range(0, 1)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -38,22 +41,24 @@ while True:
         timeText.Update(datetime.datetime.now())
         Asteper.fullAndRemove(int(values[1]) - int(values[0]) + 1)
         Asteper.into(ramdomValue - int(values[0]))
-        pathText.Update(Asteper.getCurrentPath())
+        mapText.Update(Asteper.getCurrentPath())
         if Asteper.length() > 0:
             maxInputText.Update(Asteper.length() - int(values[0]) + 1)
         maxInputText.SetFocus()
         maxInputText.Widget.select_range(0, len(values[1]))
-    elif event == 'copy':
-        pyperclip.copy(randomText.get())
     elif event == 'restep':
         Asteper.restep()
-        pathText.Update(Asteper.getCurrentPath())
+        mapText.Update(Asteper.getCurrentPath())
         maxInputText.Update(Asteper.length() - int(values[0]) + 1)
     elif event == 'print_dict':
         print(Asteper.__dict__())
+    elif event == 'import':
+        pass
+    elif event == 'export':
+        print("explorer")
     elif event == sg.WIN_CLOSED or event == 'Cancel':
         break
-    # print('You entered ', random.randrange(int(values[0]), int(values[1]) + 1))
+
     print(event, values)
 
 window.close()
